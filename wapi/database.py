141ead9f382a4e7629acb498c0c0db881b42e504
Musah95 <musafrsh95@gmail.com>
2025-07-12 01:07:16 +0100

    created runserver.py file to start the application. handled database connection settings in .env file.

--- wapi/database.py
+++ wapi/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from .config import settings
 
-SQLALCHEMY_DATABASE_URL = "postgresql+psycopg://wapi_user:g959beCyLfP4tLTyRajq8lVCNsGYS8t1@dpg-d1ood9k9c44c73fsbtgg-a.oregon-postgres.render.com/wapi"
+SQLALCHEMY_DATABASE_URL = f"postgresql+psycopg://{settings.database_username}:{settings.database_password}@{settings.database_hostname}/{settings.database_name}"
 # SQLALCHEMY_DATABASE_URL = "postgresql+psycopg://postgres:postgres@localhost:5432/wapi"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
